ba534ccac8158f21115a6b7898c26094eac49a27
Barabé Pierre <darkansloading@gmail.com>
2017-10-05 22:50:52 +0200

    Modification

--- commands/play.js
+++ commands/play.js
@@ -6,6 +6,7 @@ module.exports = class Play extends Command {
   static match (message) {
     return message.content.startsWith('!play')
   }
+<<<<<<< HEAD
 
   static action (message) {
     let voiceChannel = message.guild.channels
@@ -22,6 +23,26 @@ module.exports = class Play extends Command {
         } catch (e) {
           message.reply("je n'ai pas réussi à lire la vidéo :(")
         }
+=======
+  static action (message) {
+    let voiceChannel = message.guild.channels
+    .filter(function (channel) { return channel.type === 'voice' })
+    .first()
+  let args = message.content.split(' ')
+  voiceChannel
+    .join()
+    .then(function (connection) {
+      let stream = YoutubeStream(args[1])
+      stream.on('error', function () {
+        message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+        connection.disconnect()
+      })
+      connection
+        .playStream(stream)
+        .on('end', function () {
+          connection.disconnect()
+        })
+>>>>>>> modif
       })
   }
 }
